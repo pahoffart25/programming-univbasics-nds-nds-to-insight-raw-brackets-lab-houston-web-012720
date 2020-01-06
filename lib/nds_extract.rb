6b79da84337bbf8b23e48d73c249fa00d65bc369
Paul Hoffart <pahoffart25@gmail.com>
2020-01-06 17:51:12 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,6 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
 
+def pretty(nds)
+pp nds
+end
+
 def directors_totals(nds)
   # Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!

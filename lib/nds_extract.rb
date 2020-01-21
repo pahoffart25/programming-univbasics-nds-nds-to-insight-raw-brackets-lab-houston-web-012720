c3095193bc9ec6ae8a08034f901bfc6e7268a102
Paul Hoffart <pahoffart25@gmail.com>
2020-01-21 20:39:04 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,8 @@ def directors_totals(nds)
   total = 0
   directors_database.each do |dir_name, dir_info|
     dir_name[:movies].each do |movie|
-      
+      total += movie[:worldwide_gross]
     end
+    return total
   end
 end
